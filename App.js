69ac3c94ca47e07157f435c0b71f3f4010216131
Shafiya Ariff <shafiya.ariff23@gmail.com>
2021-03-31 23:06:35 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View style={{ backgroundColor: 'brown',flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{color:'white'}}>
+        Expense tracker lol
+      </Text>
+    </View>
+  );
+}
+
+export default YourApp;
