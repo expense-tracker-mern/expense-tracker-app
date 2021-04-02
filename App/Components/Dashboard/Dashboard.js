2da694729096befd1abaa4a63ee825c3703efced
Shafiya Ariff <shafiya.ariff23@gmail.com>
2021-04-02 19:24:54 +0530

    Dashboard scrollview fix

--- App/Components/Dashboard/Dashboard.js
+++ App/Components/Dashboard/Dashboard.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { View, Text, StyleSheet} from 'react-native';
+import { View,ScrollView, Text, StyleSheet} from 'react-native';
 import HeaderMenu from '../Header/HeaderMenu';
 
 export const Dashboard = (props) => {
     return (
-        <ScrollView style={styles.container}>
+        <ScrollView contentContainerStyle={styles.container}>
             <HeaderMenu/>
             <View>
             <Text style={{color:"black", padding: 20}}>Welcome {props .user.email} !!!!</Text>
